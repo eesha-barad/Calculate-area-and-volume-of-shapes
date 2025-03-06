0a43ed8cda39db874133142f7b873f8fbab7d459
eesha-barad <eeshabarad49@gmail.com>
2025-03-06 19:11:51 +0530

    Created abstract class Shape

---
+++ Shape.java
@@ -0,0 +1,8 @@
+//Shape.java
+
+// Abstract class Shape
+abstract class Shape {
+    protected double dim_one;
+    protected double dim_two;
+    protected double dim_three;
+    protected int num_sides;
