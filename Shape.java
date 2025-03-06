b226eb00cff9e1389de5df783064bb597b9963a8
eesha-barad <eeshabarad49@gmail.com>
2025-03-06 19:12:29 +0530

    Created constructors

--- Shape.java
+++ Shape.java
@@ -6,3 +6,17 @@ abstract class Shape {
     protected double dim_two;
     protected double dim_three;
     protected int num_sides;
+	
+// Zero Parameter Constructor
+    public Shape() {
+        dim_one = dim_two = dim_three = 0;
+        num_sides = 0;
+    }
+
+    // Parameterized Constructor
+    public Shape(double dim_one, double dim_two, double dim_three, int num_sides) {
+        this.dim_one = dim_one;
+        this.dim_two = dim_two;
+        this.dim_three = dim_three;
+        this.num_sides = num_sides;
+    }
